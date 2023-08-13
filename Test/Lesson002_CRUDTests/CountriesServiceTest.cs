6a4a028373956c38df21027bea9cb087bebd41f3
gokhanozdemir-tslnet <102428631+gokhanozdemir-tslnet@users.noreply.github.com>
2023-08-13 13:16:01 +0300

    XUNİT TEST CHECK NULL DUBLİCATE ...

--- Test/Lesson002_CRUDTests/CountriesServiceTest.cs
+++ Test/Lesson002_CRUDTests/CountriesServiceTest.cs
@@ -1,5 +1,9 @@
 ﻿
 using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+using System;
+using Xunit;
 
 namespace Lesson002_CRUDTests
 {
@@ -9,7 +13,84 @@ namespace Lesson002_CRUDTests
 
         public CountriesServiceTest()
         {
+            _countriesService = new CountriesService();
+        }
 
+        //When CountryAddRequest is null, it should ArgumentNullException
+        [Fact]
+        public void AddCountry_NullCountry() //name: MethodName_Testscenarioname
+        {
+            //Arrange
+            CountryAddRequest request = null;
+
+            //Assert
+            Assert.Throws<ArgumentNullException>(
+                //Act
+                () => _countriesService.AddCountry(request)
+                );
+
+        }
+
+        //When CountryName is null , it should throw ArgumentException
+        [Fact]
+        public void AddCountry_CountryIsNull() //name: MethodName_Testscenarioname
+        {
+            //Arrange
+            CountryAddRequest request = new CountryAddRequest
+            {
+                CountryName = null
+            };
+
+            //Assert
+            Assert.Throws<ArgumentException>(
+                //Act
+                () => _countriesService.AddCountry(request)
+                );
+
+        }
+
+        //When the CountryName isdublicate ,t sshould throw ArgumentException
+        [Fact]
+        public void AddCountry_DuplicateCountryName() //name: MethodName_Testscenarioname
+        {
+            //Arrange
+            CountryAddRequest request1 = new CountryAddRequest
+            {
+                CountryName = "USA"
+            };
+            CountryAddRequest request2 = new CountryAddRequest
+            {
+                CountryName = "USA"
+            };
+
+            //Assert
+            Assert.Throws<ArgumentException>(
+                //Act
+                () =>
+                {
+                    _countriesService.AddCountry(request1);
+                    _countriesService.AddCountry(request2);
+                }
+                );
+
+        }
+
+        //When you supply proper coountry name, it should insert
+        [Fact]
+        public void AddCountry_ProperCountryDetails() //name: MethodName_Testscenarioname
+        {
+            //Arrange
+            CountryAddRequest request1 = new CountryAddRequest
+            {
+                CountryName = "TURKEY"
+            };
+
+
+            //Act
+            CountryAddResponse response = _countriesService.AddCountry(request1);
+            
+            //Assert
+            Assert.True(response.CountryId != Guid.Empty);
         }
     }
 }
--- Test/Services/CountriesService.cs
+++ Test/Services/CountriesService.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using Entities;
 using ServiceContracts;
 using ServiceContracts.DTO;
 
@@ -7,9 +8,37 @@ namespace Services
 {
     public class CountriesService : ICountriesService
     {
+        private readonly List<Country> _countries;
+
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+        }
+
         public CountryAddResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            throw new NotImplementedException();
+            //Validation
+            if (countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+            if (countryAddRequest.CountryName == null)
+            {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+            if (_countries.Where(temp => temp.CountryName ==
+                countryAddRequest.CountryName).Count() > 0
+            )
+            {
+                throw new ArgumentException("Given country name already exists");
+            }
+
+            //Convert object from CountryAddRequest
+            Country country = countryAddRequest.ToCountry();
+            country.CountryId = Guid.NewGuid();
+            _countries.Add(country);
+
+            return country.ToCountryResponse();
         }
     }
 }
