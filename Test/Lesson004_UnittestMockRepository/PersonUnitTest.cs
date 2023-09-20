907bcf6c8ec3edd4ae53a8e2dbe9afaba834f89f
gokhanozdemir-tslnet <102428631+gokhanozdemir-tslnet@users.noreply.github.com>
2023-09-20 22:50:53 +0300

    REpository test mock

--- Test/Lesson004_UnittestMockRepository/PersonUnitTest.cs
+++ Test/Lesson004_UnittestMockRepository/PersonUnitTest.cs
@@ -1,20 +1,51 @@
+using AppService.Entities;
+using AutoFixture;
+using FluentAssertions;
+using Moq;
+using PersonService;
+using RepositoryContracts;
 using Xunit;
 
 namespace Lesson004_UnittestMockRepository
 {
     public class PersonUnitTest
     {
+        private readonly PersonService.PersonService _personservice;
+        private readonly IPersonRepository _personRepository;
+        private readonly Mock<IPersonRepository> _personRepositoryMock;
+        private readonly Fixture _fixture;
         public PersonUnitTest()
         {
-
+            _personRepositoryMock = new Mock<IPersonRepository>();
+            _personRepository = _personRepositoryMock.Object;
+            _personservice = new PersonService.PersonService(_personRepository);
+            _fixture = new Fixture();
         }
 
 
 
         [Fact]
-        public void Test1()
+        public async void AddPerson_ProperPerson_ToBeSucced()
         {
 
+
+            Person personrequest;
+
+            Person personreturn = _fixture.Build<Person>()
+               .Create();
+
+            personrequest = personreturn;
+            personrequest.Id = 0;
+
+
+            _personRepositoryMock.Setup(temp => 
+                temp.AddPerson(It.IsAny<Person>())).ReturnsAsync(
+                personreturn
+                );
+
+           var result =  await _personservice.AddPerson(personrequest);
+
+            result.Should().Be(result.Id > 0);
         }
     }
 }
