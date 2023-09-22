163ba472eb2b2894e1ec9217656cb25e32f667b4
TESLANET <gokhanozdemir_77@hotmail.com>
2023-09-22 23:06:42 +0300

    net 7

--- DesingPatterns/DPConsole/.vshistory/Program.cs/2023-09-22_23_01_47_750.cs
+++ DesingPatterns/DPConsole/.vshistory/Program.cs/2023-09-22_23_01_47_750.cs
@@ -1,15 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-
-using DPConsole.Creational.Singleton.RealWorldExample;
-
-//var singletonOperation = Singleton.GetInstance();
-//Console.WriteLine(singletonOperation.MySetting.MyClassPropId);
-//Console.WriteLine(singletonOperation.GuidId);
-//var singletonOperation2 = Singleton.GetInstance();
-//Console.WriteLine(singletonOperation2.MySetting.MyClassPropId);
-//Console.WriteLine(singletonOperation2.GuidId);
+var singletonOperation = DPConsole.Creational.Singleton.GetInstance();
+Console.WriteLine(singletonOperation.MySetting.MyClassPropId);
+Console.WriteLine(singletonOperation.GuidId);
+var singletonOperation2 = Singleton.GetInstance();
+Console.WriteLine(singletonOperation2.MySetting.MyClassPropId);
+Console.WriteLine(singletonOperation2.GuidId);
 
 //Singleton Real World
 Console.WriteLine("Request1----" + DBConfiguration.GetDBConfiguration().DbConnection.ConnectionString);
