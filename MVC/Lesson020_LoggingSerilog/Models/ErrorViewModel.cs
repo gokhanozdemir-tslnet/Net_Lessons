83e65ff885b4c1dcb2343a6ee66b606743ca7da1
TESLANET <gokhanozdemir_77@hotmail.com>
2023-11-04 16:21:55 +0300

    serilog

---
+++ MVC/Lesson020_LoggingSerilog/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson020_LoggingSerilog.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
